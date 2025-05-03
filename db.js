86a8a55e486927ece8a927781c5e22414748361b
drelocatelli <andressalocatelli@hotmail.com>
2025-05-03 13:47:11 -0300

    remake security info

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const { Sequelize } = require('sequelize');
 const { config } = require('dotenv');
-const mysql = require('mysql2')
+const mysql = require('mysql2');
 
 config(); // Carrega variáveis de ambiente
 
 // Valida variáveis de ambiente essenciais
-['DB_NAME', 'DB_USER', 'DB_PASSWORD', 'DB_HOST', 'DB_DIALECT'].forEach((key) => {
+['DB_NAME', 'DB_USER', 'DB_PASSWORD', 'DB_HOST', 'DB_DIALECT', 'DB_PORT'].forEach((key) => {
   if (!process.env[key]) {
     throw new Error(`Variável de ambiente ${key} não definida.`);
   }
@@ -16,14 +16,14 @@ async function createDb() {
     const conn = mysql.createConnection({
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD
-    })
-    
-    conn.query(`CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME}`);
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT // <-- Aqui
+    });
 
+    conn.query(`CREATE DATABASE IF NOT EXISTS \`${process.env.DB_NAME}\``); // safer with backticks
     console.log('Banco de dados criado com sucesso!');
     conn.end();
-  } catch(err) {
+  } catch (err) {
     console.error('Erro ao criar banco de dados');
     console.error(err);
   }
@@ -33,8 +33,9 @@ createDb();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT, // <-- Aqui também
   dialect: process.env.DB_DIALECT,
-  logging: false, // Desativa logs SQL
+  logging: false,
 });
 
 module.exports = sequelize;
